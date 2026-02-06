6b0074229bad6479fdf9162404681dedf688b2e4
Deepak Singh <deepakrajput.ds2008@gmail.com>
2026-02-07 00:17:35 +0530

    Add basic math operations program

---
+++ basic_math_operations.py
@@ -0,0 +1,17 @@
+# Program to add and subtract two numbers
+
+# Taking first number from user
+num1 = int(input("Enter first number: "))
+
+# Taking second number from user
+num2 = int(input("Enter second number: "))
+
+# Adding two numbers
+addition = num1 + num2
+
+# Subtracting second number from first number
+subtraction = num1 - num2
+
+# Displaying the results
+print("Addition =", addition)
+print("Subtraction =", subtraction)
