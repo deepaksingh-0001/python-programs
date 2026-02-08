3c6d9a7734ca0b64bfbe4dc7799767866d34f279
Deepak Singh <deepakrajput.ds2008@gmail.com>
2026-02-08 17:24:06 +0530

    Add program to generate first N natural numbers

---
+++ natural_numbers.py
@@ -0,0 +1,8 @@
+# Program to generate first N natural numbers
+
+# Taking input from the user
+n = int(input("Enter the ending number: "))
+
+# Loop to print natural numbers from 1 to n
+for i in range(1, n + 1):
+    print(i, end=" ")   # end is used to print numbers in one line
