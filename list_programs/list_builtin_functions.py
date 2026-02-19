e1d2de514bae733a84fe4940014ff74a4c68f0cf
Deepak Singh <deepakrajput.ds2008@gmail.com>
2026-02-19 23:43:04 +0530

    Add Python list built-in functions

---
+++ list_programs/list_builtin_functions.py
@@ -0,0 +1,11 @@
+# List Built-in Functions
+
+l1 = [3, 4, 8, 0, 8, 8]
+
+print("Length  :", len(l1))
+print("Max     :", max(l1))
+print("Min     :", min(l1))
+print("Sum     :", sum(l1))
+print("Sorted  :", sorted(l1))
+print("Any     :", any(l1))
+print("All     :", all(l1))
