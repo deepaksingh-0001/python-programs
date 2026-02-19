c2713447557b69489562c58ce79c56d6bfb931f3
Deepak Singh <deepakrajput.ds2008@gmail.com>
2026-02-20 00:54:16 +0530

    Add python list conversion and enumerate functions program

---
+++ list_programs/list_conversion_enumerate.py
@@ -0,0 +1,16 @@
+# Conversion and Iterator Functions (Beginner Friendly)
+
+# List declaration
+l1 = [3, 4, 8, 0, 8, 8]
+
+#convert Tuple to list
+s = ("deepak", "singh", "rajput")
+print("Tuple to List:", list(s))
+
+#reverse List
+print("Reversed List:", list(reversed(l1)))
+
+# Enumerate example
+list1 = ["apple", "banana", "mango"]
+
+print("Enumerate Output:", list(enumerate(list1)))
