991b2fa8f9a593aaad4b6e925d07659890d736c3
Deepak Singh <deepakrajput.ds2008@gmail.com>
2026-02-22 01:55:42 +0530

    Add python program to concatenate two lists using user input

---
+++ list_programs/add_two_lists.py
@@ -0,0 +1,6 @@
+# Add two lists using (+) with user input
+
+lst1 =list(map(int, input("enter list 1: ").split()))
+lst2 =list(map(int, input("enter list 2: ").split()))
+final_list = lst1 + lst2
+print("Combined list:", final_list)
