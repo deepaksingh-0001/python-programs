28bda93f87adabcae644a60febc57373886bed7c
Deepak Singh <deepakrajput.ds2008@gmail.com>
2026-02-24 00:14:41 +0530

    add python program to decribe difference between string,list,and tuple

---
+++ str_list_tuple_diff.py
@@ -0,0 +1,9 @@
+# Difference between string, list and tuple
+
+s = "1020"        # string (characters inside quotes)
+l = [10, 20]      # list (elements inside square brackets)
+t = (10,)         # single element tuple (comma required)
+
+print("Type of s:", type(s))
+print("Type of l:", type(l))
+print("Type of t:", type(t))
